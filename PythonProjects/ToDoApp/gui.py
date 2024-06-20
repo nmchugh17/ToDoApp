ff6f0456b58545f43b4a157868da8928cb17ab63
nmchugh17 <nmchu17@gmail.com>
2024-06-20 20:42:18 +0100

    Check filepath exists and image files included

--- PythonProjects/ToDoApp/gui.py
+++ PythonProjects/ToDoApp/gui.py
@@ -1,18 +1,25 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("Black")
 
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button(key ="Add", size=10, image_source="add.png", mouseover_colors="LightBlue2", tooltip="Add todo")
-list_box = sg.Listbox(values = functions.get_todos(), key="todos",
-                      enable_events=True, size=[45,10])
+# add_button = sg.Button(key="Add", size=10, image_source="add.png", mouseover_colors="LightBlue2", tooltip="Add todo")
+add_button = sg.Button("Add", key="Add", size=10)
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
 
-complete_button = sg.Button(key="Complete", image_source="complete.png", tooltip="Complete todo")
+# complete_button = sg.Button(key="Complete", image_source="complete.png", tooltip="Complete todo")
+complete_button = sg.Button("Complete", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
@@ -25,7 +32,7 @@ window = sg.Window('My To-Do App',
 
 while True:
     event, values = window.read(timeout=10)
-    window["clock"].update(value=time.strftime(("%b %d, %Y %H:%M:%S")))
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
     match event:
